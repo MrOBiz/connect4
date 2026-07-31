447f619e91205ba6379785f68b96cb45a81622b1
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-31 09:43:10 +0200

    Added 1d tic tac toe

---
+++ practice-for-connect-4/1d-tic-tac-toe.js
@@ -0,0 +1,10 @@
+function Game() {
+    const board = [];
+
+    createBoard(cells) = () => {
+            for(let i = 0; i < cells; i++){
+                board.push(cell);
+            }
+        return board;
+    }
+}
