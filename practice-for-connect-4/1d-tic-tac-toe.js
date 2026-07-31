227822f68a37fd784292dc2827efe2f1772d1112
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-31 10:00:25 +0200

    Added Game obj to 1d TTT

--- practice-for-connect-4/1d-tic-tac-toe.js
+++ practice-for-connect-4/1d-tic-tac-toe.js
@@ -1,10 +1,31 @@
 function Game() {
     const board = [];
 
-    createBoard(cells) = () => {
+    const createBoard = (cells) => {
             for(let i = 0; i < cells; i++){
                 board.push(cell);
             }
         return board;
     }
+
+    const getTurn = () => {
+        const Players = [
+            {
+                name: P1,
+                token: "1"
+            },
+            {
+                name: P2,
+                token: "2"
+            }
+        ]
+
+        let activeP = Players[0];
+
+        const switchTurn = () => {
+            activeP = activeP === Players[0]? Players[1] : Players[0];
+        }
+    }
+
+    return {createBoard, getTurn};
 }
